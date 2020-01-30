dce446098c721c7326e0ed7d2da966ac7a0b4825
Fred Rondina <fredrondina96@gmail.com>
2020-01-30 14:43:18 -0700

    Added db for shelter creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/shelters', to: 'shelters#index'
   get '/', to: 'welcome#index'
-
+  get '/shelters/new', to: 'shelters#new'
+  post '/shelters', to: 'shelters#create'
 end
